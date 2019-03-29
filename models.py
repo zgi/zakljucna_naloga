3bd61e0961c19cc9c7a0b1d2ec82b864d6c18ff9
Gregor Žagar <gregor.zagar@gmail.com>
2019-03-29 14:31:23 +0100

    first commit

---
+++ models.py
@@ -0,0 +1,11 @@
+from google.appengine.ext import ndb
+
+class Mail(ndb.Model):
+    sporocilo = ndb.StringProperty()
+    zadeva = ndb.StringProperty()
+    posiljatelj = ndb.StringProperty()
+    naslovnik = ndb.StringProperty()
+    izbrisano = ndb.BooleanProperty(default=False)
+    prebrano = ndb.BooleanProperty(default=False)
+    poslano = ndb.BooleanProperty(default=False)
+    nastanek = ndb.DateTimeProperty(auto_now_add=True)
